5ff5d44c07facd1bab3c81ccf3b7ece41ee5c6d2
saqlain615 <mushtaqasm786@gmail.com>
2022-10-03 22:51:06 +0500

    add map

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,6 +5,8 @@
         @component('components.main-carousel')@endcomponent
         <!-- programs -->
         @component('components.programs')@endcomponent
+        <!-- map -->
+        @component('components.map')@endcomponent
         <!-- footer along with script tags -->
     </main>
     @include('includes.footer')
