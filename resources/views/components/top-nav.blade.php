a108994939ce97785cf48e95574e96223682a605
saqlain615 <mushtaqasm786@gmail.com>
2022-10-04 22:36:05 +0500

    add some missing bootstrap file and set top bar slider

--- resources/views/components/top-nav.blade.php
+++ resources/views/components/top-nav.blade.php
@@ -1,61 +1,79 @@
 <div class="header-top">
-        <div class="container">
-            <div class="header-row">
-
-            <div class="header-top-strip">
-            <div class="header-top-left">
-                <div class="header-top-button">
-                    <span>Announcement</span>
+    <div class="container">
+            <div class="row">
+                <div class="col-md-2 bg-danger text-center p-2 text-white">
+                        Announments
                 </div>
-                <div class="header-top-left-text">
-                    <marquee>This is announcement and we are announcing right now</marquee>
-                </div>
-                <div class="header-top-left-nav-icons">
-                    <div class="navigation">
-                        <span> < </span>
-                        <span> > </span>
+                <div class="col-md-7">
+                  <div id="demo" class="carousel slide" data-ride="carousel">
+                    
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <div class="carousel-caption">
+                          <p>Sindh education foundation</p>
+                        </div>   
+                      </div>
+                      <div class="carousel-item">
+                        <div class="carousel-caption">
+                          <p>User</p>
+                        </div>   
+                      </div>
+                      <div class="carousel-item">
+                        <div class="carousel-caption">
+                          <p>New York</p>
+                        </div>   
+                      </div>
                     </div>
+                    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                      <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#demo" data-slide="next">
+                      <span class="carousel-control-next-icon"></span>
+                    </a>
+                  </div>
                 </div>
-                <div class="header-top-right">
-                    <div class="header-top-right-row">
-                        <div class="header-top-right-count">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-                              <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-                              <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-                            </svg>
-                        </div>
-                        <span class="block"> | </span>
-                        
-                        <div class="header-top-right-icons-wrapper">
-                            <div class="header-top-right-icons">
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <i class="bi bi-google"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                    <i class="bi bi-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="bi bi-twitter"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="bi bi-instagram"></i>
-                                        </a>
-                                    </li>
-                                </ul>
+                
+                    <div class="col-md-3 header-top-right">
+                        <div class="header-top-right-row">
+                            <div class="header-top-right-count">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                </svg>
+                            </div>
+                            <span class="block"> | </span>
+                            
+                            <div class="header-top-right-icons-wrapper">
+                                <div class="header-top-right-icons">
+                                    <ul>
+                                        <li>
+                                            <a href="#">
+                                                <i class="bi bi-google"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                        <i class="bi bi-facebook"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="bi bi-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="bi bi-instagram"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                
+
             </div>
         </div>
-        </div>
-        </div>
+        
     </div>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,9 +1,14 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>new site</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('b5/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('b5/bootstrap.bundle.min.js') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/custom.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('slick/slick.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('slick/slick-theme.css') }}">
+   <!-- for header aunoument slider -->
+    <script type='text/javascript' src="{{asset('jquery/jquery.slim.min.js') }}" ></script>
+    <script type='text/javascript' src="{{asset('jquery/bootstrap.bundle.js') }}" ></script>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>new site</title>
+   
