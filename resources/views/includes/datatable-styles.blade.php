875718327911b8f2f90fea1e5910d29b199a2d75
zowhair <zowhair@gmail.com>
2022-10-15 18:16:01 +0500

    refactoring

---
+++ resources/views/includes/datatable-styles.blade.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.min.css" integrity="sha512-gMjQeDaELJ0ryCI+FtItusU9MkAifCZcGq789FrzkiM49D8lbDhoaUaIX4ASU187wofMNlgBJ4ckbrXM9sE6Pg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('datatable/dataTables.bootstrap5.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('datatable/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('b5/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter%3A300%2C400%2C500%2C600%2C700%2C800%7CRoboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto%20Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CHeebo%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;display=swap">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/coppy/blog.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/coppy/master.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('owl/owl.carousel.css') }}">
--- resources/views/pages/tender.blade.php
+++ resources/views/pages/tender.blade.php
@@ -1,8 +1,17 @@
-@extends('layout.app')
+@extends('layout.tender-datatable')
+@section('styles')
+    @push('includes.datatable-styles')
+    @endpush
+@stop
 @section('content')
     @component('components.nav')@endcomponent
     <main>
         @component('components.tender-datatables')@endcomponent
     </main>
-    @include('includes.footer')
+    @include('includes.datatable-footer')
+    
+    @section('scripts')
+        @push('includes.datatable-scripts')
+        @endpush
+    @stop
 @stop
