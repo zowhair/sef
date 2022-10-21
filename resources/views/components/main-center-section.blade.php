fc3a317ca431e382136c9256a21744f05c2a3fbe
saqlain615 <mushtaqasm786@gmail.com>
2022-10-21 19:54:01 +0500

    set compnent design

--- resources/views/components/main-center-section.blade.php
+++ resources/views/components/main-center-section.blade.php
@@ -1,21 +1,22 @@
 <div class="row">
     <div class="col-8 main-center-section-left">
         <div class="main-center-title">
-            <h1>Programs</h1>
+            <h4>Programs</h4>
         </div>
         <div class="widget-title"></div>
     </div>
     <div class="col-4 main-center-section-right">
         <div class="main-center-section-right-top">
             <div class="main-center-right-title">
-                <h3>Programs</h3>
+                <h5>Programs</h5>
             </div>
             <div class="widget-title"></div>
             <div class="main-center-right-list">
                 <ul>
                     @for($i=0;$i<4;$i++)
                     <li>
-                        <a href="#">This is new link</a>
+                        <a href="#">Adopt-a-School-Program (AASP)</a>
+                        <div class="border_black"></div>
                     </li>
                     @endfor
                 </ul>
@@ -23,18 +24,22 @@
         </div>
         <div class="main-center-section-right-top">
             <div class="main-center-right-title">
-                <h3>Recent News</h3>
+                <h5>Recent News</h5>
             </div>
             <div class="widget-title"></div>
             <div class="main-center-right-list">
                 <ul>
-                    @for($i=0;$i<4;$i++)
-                    <li>
-                        <a href="#">This is new link</a>
-                    </li>
-                    @endfor
+                    <p>
+                        @for($i=0;$i<2;$i++)
+                            <li>
+                                Planning and Organizing Events, Seminars, Workshops
+                            </li>
+                        @endfor
+                    </p>
                 </ul>
+                <div class="border_black"></div>
             </div>
+            
         </div>
     </div>
 </div>
